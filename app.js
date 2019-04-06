ef0d371e835ee7743ff93de0bc3d344e81148d00
varaprasadh <varaprasadh.a@gmail.com>
2019-04-06 10:30:52 +0530

    made as cli

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+#!/usr/bin/env node
 const express=require('express');
 const app=express();
 const path=require('path');
 const port= process.env.PORT||8080;
 const fs=require('fs');
-const os=require('os');
 const url=require('url');
 
 app.use(express.static(path.join(__dirname,'public')));
@@ -11,25 +11,24 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname);
 
 app.get('/',(req,res)=>{
-   var rootlist=fs.readdirSync(__dirname);
+    var cwd = process.cwd();
+    var rootlist = fs.readdirSync(cwd);
    var resolvedPath='';
    res.render('index',{ rootpath:resolvedPath, rootlist,home:true});
 }) 
 
-//focus here ..
 app.get(/(\/\w)*\/\w+/,(req,res)=>{
     var reqUrl=url.parse(req.url);
     // console.log(reqUrl);
     var relPath = reqUrl.path;
     console.log(relPath);
-    var resolvedPath=relPath.replace('/',path.sep);
+    var cwd=process.cwd();
+    var resolvedPath=relPath.replace('/',path.sep).replace('%20',' ');
    console.log("resolved"+resolvedPath);
-
-    var state = fs.statSync(path.join(__dirname,resolvedPath));
+    var state = fs.statSync(path.join(cwd,resolvedPath));
     console.log(state);
-    
     if(state.isDirectory()){
-             var rootlist=fs.readdirSync(path.join(__dirname,resolvedPath));
+        var rootlist = fs.readdirSync(path.join(cwd,resolvedPath));
               res.render('index', {
                   rootpath: relPath,
                   rootlist,
@@ -37,11 +36,11 @@ app.get(/(\/\w)*\/\w+/,(req,res)=>{
               });
     }
     else{
-       var rs=fs.createReadStream(path.join(__dirname,resolvedPath));
+        var rs = fs.createReadStream(path.join(cwd,resolvedPath));
        rs.pipe(res)
         }
-  
 })
-app.listen(port, "192.168.0.12", () => {
-    console.log(`server started in ${path.basename(__dirname)}`);
+app.listen(port, () => {
+    console.log(`server started in ${path.basename(__dirname)} at port:${port}`);
+    console.log(process.cwd());
 })
